06c26054b983f83848879b283f3badf02f486886
malibinYun <nightmare73@naver.com>
2021-04-05 23:28:19 +0900

    [Feat] 오토 레이아웃 스토리보드로 짬

--- BoostCourseProjects/BoostCourseMediaPlayer/BoostCourseMediaPlayer/ViewController.swift
+++ BoostCourseProjects/BoostCourseMediaPlayer/BoostCourseMediaPlayer/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addViews()
+//        addViews()
         self.initializePlayer()
     }
     
